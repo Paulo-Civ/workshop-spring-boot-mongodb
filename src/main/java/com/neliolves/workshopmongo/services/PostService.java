82192ccfe130acbe212b8e3103f188a1b024a95b
Paulo Civardi <paulocivardi@gmail.com>
2024-01-16 20:25:13 -0400

    Consulta simples com @Query

--- src/main/java/com/neliolves/workshopmongo/services/PostService.java
+++ src/main/java/com/neliolves/workshopmongo/services/PostService.java
@@ -22,7 +22,8 @@ public class PostService {
 	}
 	
 	public List<Post> findByTitle(String text) {
-		return postRepository.findByTitleContainingIgnoreCase(text);
+		//return postRepository.findByTitleContainingIgnoreCase(text);
+		return postRepository.searchTitle(text);
 	}
 
 }
