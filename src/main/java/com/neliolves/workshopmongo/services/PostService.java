0f1f2f2f14f57099a9e895df36b6699fb31dce0b
Paulo Civardi <paulocivardi@gmail.com>
2024-01-17 17:11:43 -0400

    Consulta com vários critérios

--- src/main/java/com/neliolves/workshopmongo/services/PostService.java
+++ src/main/java/com/neliolves/workshopmongo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.neliolves.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +25,12 @@ public class PostService {
 	public List<Post> findByTitle(String text) {
 		//return postRepository.findByTitleContainingIgnoreCase(text);
 		return postRepository.searchTitle(text);
+	}
+	
+	public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return postRepository.fullSearch(text, minDate, maxDate);
+
 	}
 
 }
